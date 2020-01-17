6366f706b0f379d59a7420d92200adc394eb74b7
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2020-01-17 01:52:43 +0100

    Add set_seed()

--- R/helpers.R
+++ R/helpers.R
@@ -24,21 +24,6 @@ skip_test_if_old_R_version <- function(min_R_version = "3.6") {
   }
 }
 
-# Wrapper for setting seed with the sample generator for R versions <3.6
-# Used for unittests
-# Partly contributed by R. Mark Sharp
-set_seed_for_R_compatibility <- function(seed = 1) {
-  if ((getRversion()$major == 3 &&
-       getRversion()$minor >= 6) ||
-      getRversion()$major > 3) {
-    args <- list(seed, sample.kind = "Rounding")
-  } else {
-    args <- list(seed)
-  }
-  suppressWarnings(do.call(set.seed, args))
-}
-
-
 # Remove NAs and empty "" names
 non_empty_names <- function(x) {
   ns <- names(x)
---
+++ R/set_seed.R
@@ -0,0 +1,24 @@
+
+#' @title Set random seed
+#' @description
+#'  \Sexpr[results=rd, stage=render]{lifecycle::badge("experimental")}
+#'
+#'  In order for tests to be compatible with R versions \code{< 3.6.0},
+#'  we set the \code{sample.kind} argument in \code{\link[base:set.seed]{set.seed()}}
+#'  to \code{"Rounding"} when using R versions \code{>= 3.6.0}.
+#' @param seed Random seed.
+#' @param ... Named arguments to \code{\link[base:set.seed]{set.seed()}}.
+#' @author Ludvig Renbo Olsen, \email{r-pkgs@@ludvigolsen.dk}
+#' @author R. Mark Sharp
+#' @details
+#'  Initially contributed by R. Mark Sharp (github: @@rmsharp).
+set_seed <- function(seed = 42, ...) {
+  if ((getRversion()$major == 3 &&
+       getRversion()$minor >= 6) ||
+      getRversion()$major > 3) {
+    args <- list(seed, sample.kind = "Rounding", ...)
+  } else {
+    args <- list(seed, ...)
+  }
+  suppressWarnings(do.call(set.seed, args))
+}
