59f7b17a8c53f746183e93c4766ec6c82cd0f60e
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2020-01-18 14:25:06 +0100

    fixes tes

--- tests/testthat/test_gxs_function.R
+++ tests/testthat/test_gxs_function.R
@@ -48,52 +48,36 @@ test_that("gxs_function() works", {
     output
   }
 
-  # TODO
-  # expect_equal(
-  #   gxs_function(fn, list(
-  #     "a" = list(1, 2),
-  #     a_fn = list(mean, sum),
-  #     "b" = list("ah", "eh")
-  #   ),
-  #   out = "return"),
-  #   c(
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 2, a_fn = mean, b",
-  #            " = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:",
-  #            "\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n",
-  #            " * Variable 'b': Must have length 2, but has length 1.\")),",
-  #            "\n  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = sum, b ",
-  #            "= \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:",
-  #            "\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n ",
-  #            "* Variable 'b': Must have length 2, but has length 1.\")),\n",
-  #            "  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b",
-  #            " = \"eh\")),\n  xpectr::strip(paste0(\"2 assertions failed:",
-  #            "\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n",
-  #            " * Variable 'b': Must have length 2, but has length 1.\")),",
-  #            "\n  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = NULL, a_fn = mean",
-  #            ", b = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions faile",
-  #            "d:\\n * Variable 'a': Must be of type 'numb\",\n         \"e",
-  #            "r', not 'NULL'.\\n * Variable 'b': Must have length 2, but h",
-  #            "\",\n         \"as length 1.\")),\n  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = NULL, b",
-  #            " = \"ah\")),\n  xpectr::strip(paste0(\"3 assertions failed:",
-  #            "\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n",
-  #            " * Variable 'a_fn': Must be a function, not 'NULL'.\\n * V\"",
-  #            ",\n         \"ariable 'b': Must have length 2, but has lengt",
-  #            "h 1.\")),\n  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b",
-  #            " = NULL)),\n  xpectr::strip(paste0(\"2 assertions failed:\\n",
-  #            " * Variable 'a': Element 0 is not >= 4\",\n         \".\\n *",
-  #            "* Variable 'b': Must be of type 'character', not 'NULL'.\")),",
-  #            "\n  fixed = TRUE)"),
-  #     paste0("expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b",
-  #            " = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:",
-  #            "\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n",
-  #            " * Variable 'b': Must have length 2, but has length 1.\")),",
-  #            "\n  fixed = TRUE)")
-  #   ), fixed = TRUE
-  # )
+  current_envir <- sys.frame(which = sys.nframe())
+
+  expected_tests <- c(
+    "expect_error(\n  xpectr::strip_msg(fn(a = 2, a_fn = mean, b = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'b': Must have length 2, but has length 1.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = sum, b = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'b': Must have length 2, but has length 1.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b = \"eh\")),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'b': Must have length 2, but has length 1.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = NULL, a_fn = mean, b = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Must be of type 'numb\",\n         \"er', not 'NULL'.\\n * Variable 'b': Must have length 2, but h\",\n         \"as length 1.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = NULL, b = \"ah\")),\n  xpectr::strip(paste0(\"3 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'a_fn': Must be a function, not 'NULL'.\\n * V\",\n         \"ariable 'b': Must have length 2, but has length 1.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b = NULL)),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'b': Must be of type 'character', not 'NULL'.\")),\n  fixed = TRUE)",
+    "expect_error(\n  xpectr::strip_msg(fn(a = 1, a_fn = mean, b = \"ah\")),\n  xpectr::strip(paste0(\"2 assertions failed:\\n * Variable 'a': Element 0 is not >= 4\",\n         \".\\n * Variable 'b': Must have length 2, but has length 1.\")),\n  fixed = TRUE)"
+  )
+
+  expect_equal(
+    strip(gxs_function(fn, list(
+        "a" = list(1, 2),
+        a_fn = list(mean, sum),
+        "b" = list("ah", "eh")
+      ),
+      out = "return")),
+    strip(expected_tests),
+    fixed = TRUE)
+  expect_equal(
+  length( gxs_function(fn, list(
+        "a" = list(1, 2),
+        a_fn = list(mean, sum),
+        "b" = list("ah", "eh")
+      ),
+      out = "return") ),
+  7L)
+
+  eval_expectations(expected_tests,envir = current_envir)
 
 })
